572e307d7fa63bbbca91b7c4e3cb4b9c7aeb3ef9
Pegah Eizad <peizadkh@gmail.com>
2018-05-17 11:36:45 -0400

    Nav JS working
    Finished component.

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -4,5 +4,25 @@ import './navigation-bar.css';
 
 // The NavigationBar component goes here.  It should be the default export.
 export default function NavigationBar(props){
-  
-}
+   const links = props.links.map((link, index) =>
+      <li>
+         <a key={index} href={link.href}>
+            {link.text}
+         </a>
+      </li>
+   );
+   return(
+
+     <nav className="navigation-bar navigation-bar-nav">
+       <h1>
+         {props.title}
+       </h1>
+         {links}
+     </nav>
+   );
+};
+
+NavigationBar.defaultProps={
+  title: '',
+  links: ''
+};
